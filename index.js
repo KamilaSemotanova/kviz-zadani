fe499762b82d6f09d119112729b7c2c0794e5938
Kamila Semotánová <kamila@Kamilas-MacBook-Air.local>
2023-03-25 11:27:24 +0100

    solution

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log('funguju!');
+
+const questionElm = document.querySelector('.question');
+questionElm.textContent =
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
+
+const answer__user = prompt(
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.',
+);
+
+console.log(answer__user);
+
+const answerTextElm = document.querySelector('.answer__text');
+answerTextElm.textContent = answer__user;
+
+const answerElm = document.querySelector('.answer');
+
+if (answer__user.toLowerCase() === 'bell') {
+  answerElm.classList.add('answer--correct');
+} else {
+  answerElm.classList.add('answer--wrong');
+}
